74c35c34ef95d4f7877afadd75541b323b1cd49a
JakeLi22 <56609057+JakeLi22@users.noreply.github.com>
2021-03-25 09:44:41 -0400

    help

--- src/js/index.js
+++ src/js/index.js
@@ -1,11 +1,19 @@
-console.log("connected");
-
 //question array
-const questions [
-    'Question 1?',
-    'Question 2?',
-    'Question 3?',
-    'Question 4?',
-    'Question 5?',
-    'Question 6?',
-]; 
+
+const questions = [
+  { text: "question1" },
+  /*{ text: "question2" },
+  { text: "question3" },
+  { text: "question4" },
+  { text: "question5" },
+  { text: "question6" },
+  { text: "question7" },*/
+];
+
+const test = "word";
+
+const questionTitles = document.querySelectorAll("#title h1 .question");
+console.log(titles);
+
+const title = document.querySelector("#title");
+title.innerHTML = " <h1> test</h1>";
