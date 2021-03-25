ac5f729b0f1d12ab7d4c8787a59d5497c28e89ee
JakeLi22 <56609057+JakeLi22@users.noreply.github.com>
2021-03-25 09:57:51 -0400

    title
    work on changing the question and display the array.

--- src/js/index.js
+++ src/js/index.js
@@ -1,19 +1,19 @@
 //question array
 
 const questions = [
-  { text: "question1" },
-  /*{ text: "question2" },
-  { text: "question3" },
+  { text: "question1?" },
+  { text: "question2?" },
+  /*{ text: "question3" },
   { text: "question4" },
   { text: "question5" },
   { text: "question6" },
   { text: "question7" },*/
 ];
 
-const test = "word";
-
-const questionTitles = document.querySelectorAll("#title h1 .question");
-console.log(titles);
-
 const title = document.querySelector("#title");
-title.innerHTML = " <h1> test</h1>";
+
+title.textContent = "word";
+//now get the title to display first id of the array
+//function to display the text of the object by id
+//write a function that adds 1 to the id when you press the next button
+//listen for click event
