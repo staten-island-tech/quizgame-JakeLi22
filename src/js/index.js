5ef4e63c9d7ad0550b288af627ab315f35fb3588
JakeLi22 <56609057+JakeLi22@users.noreply.github.com>
2021-04-06 00:45:08 -0400

    start button connected

--- src/js/index.js
+++ src/js/index.js
@@ -19,3 +19,25 @@ const test1 = ["jake", "Jake", "JAKE"];
 //function to display the text of the object by id
 //write a function that adds 1 to the id when you press the next button
 //listen for click event
+
+//start quiz
+const startButton = document.getElementById("start-btn");
+const quizPanel = document.getElementById("quiz-panel");
+
+startButton.addEventListener("click", startQuiz);
+
+function startQuiz() {
+  console.log("started");
+  //startButton.classList.add("hide"); //to hide the element
+  //might wanna come up with you own method, but this is a way
+  //show the quiz panel
+  nextQuestion();
+}
+//next question
+
+function nextQuestion() {}
+
+//make a quesion array
+
+//when you select an answer
+function choiceSelect() {}
