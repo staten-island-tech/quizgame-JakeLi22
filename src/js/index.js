d972db171638347a70bc3eabd670cea97c18209d
JakeLi22 <56609057+JakeLi22@users.noreply.github.com>
2021-04-06 10:00:45 -0400

    edits!

--- src/js/index.js
+++ src/js/index.js
@@ -1,28 +1,28 @@
 //question array
 
-const questions = [
-  { text: "question1?" },
-  { text: "question2?" },
-  /*{ text: "question3" },
-  { text: "question4" },
-  { text: "question5" },
-  { text: "question6" },
-  { text: "question7" },*/
+const quizQuestions = [
+  {
+    question: "question1?",
+    choices: ["choice1", "choice2", "choice3", "choice4"],
+    answer: 1,
+  },
+  {
+    question: "question2?",
+    choices: ["choice1", "choice2", "choice3", "choice4"],
+    answer: 1,
+  },
 ];
 
-const title = document.querySelector("#title");
+const quizTitle = document.getElementById("quiz-title");
+const startButton = document.getElementById("start-btn");
+const quizPanel = document.getElementById("quiz-panel");
 
-title.innerText = test;
-let test = "word";
-const test1 = ["jake", "Jake", "JAKE"];
 //now get the title to display first id of the array
 //function to display the text of the object by id
 //write a function that adds 1 to the id when you press the next button
 //listen for click event
 
 //start quiz
-const startButton = document.getElementById("start-btn");
-const quizPanel = document.getElementById("quiz-panel");
 
 startButton.addEventListener("click", startQuiz);
 
@@ -41,3 +41,7 @@ function nextQuestion() {}
 
 //when you select an answer
 function choiceSelect() {}
+
+//display question and answers
+quizTitle.innerText = test;
+let test = "woisd"; //doesn't work
