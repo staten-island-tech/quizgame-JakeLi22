74bbcca3783758cd12e9d5f695c122286ed758f9
JakeLi22 <56609057+JakeLi22@users.noreply.github.com>
2021-04-09 01:59:29 -0400

    title changer

--- src/js/index.js
+++ src/js/index.js
@@ -2,11 +2,37 @@ const quizTitle = document.getElementById("quiz-title");
 const startButton = document.getElementById("start-btn");
 const nextButton = document.getElementById("next-btn");
 const quizPanel = document.getElementById("quiz-panel");
+const choices = [
+  {
+    question: "when season were you born?",
+    answers: [
+      { text: "summer", correct: true },
+      { text: "winter", correct: false },
+    ],
+  },
+  {
+    question: "when month were you born?",
+    answers: [
+      { text: "june", correct: true },
+      { text: "july", correct: false },
+    ],
+  },
+  {
+    question: "when day were you born?",
+    answers: [
+      { text: "19", correct: true },
+      { text: "20", correct: false },
+    ],
+  },
+];
+
+const test = ["one", "two", "three", "four"];
+quizTitle.innerText = test[0];
 
 //start quiz
 
 startButton.addEventListener("click", startQuiz);
-
+nextButton.addEventListener("click", nextQuestion);
 function startQuiz() {
   console.log("started");
   //startButton.classList.add("hide"); //to hide the element
@@ -17,28 +43,21 @@ function startQuiz() {
 //next question
 
 function nextQuestion() {
-  quizTitle.innerText = "test";
+  //quizTitle.innerText = choices.question;
+  const searchTerm = quizTitle.innerText; //first term of the array
+  var index = test.indexOf(searchTerm) + 1; //adds one to the array
+  if (index === test.length) index = 0; //instead of index = 0; set it to the score
+  //once the end of the array is reached go back to teh beginining
+  const result = test[index]; //result is the change in id
+  quizTitle.innerText = result; //displays the result
 }
+//use id ++ to increment by 1
 
 //make a quesion array
 
 //when you select an answer
-function choiceSelect() {
-  question: "when season were you born?";
-  answers: [
-    { text: "summer", correct: true },
-    { text: "winter", correct: false },
-  ];
-  question: "when month were you born?";
-  answers: [
-    { text: "june", correct: true },
-    { text: "july", correct: false },
-  ];
-  question: "when day were you born?";
-  answers: [
-    { text: "19", correct: true },
-    { text: "20", correct: false },
-  ];
-}
+function choiceSelect() {}
 
 //display question and answers
+
+//question array {
