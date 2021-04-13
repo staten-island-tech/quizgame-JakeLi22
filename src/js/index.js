0f41b51d904c38832e3579915adf4d229321dd96
JakeLi22 <56609057+JakeLi22@users.noreply.github.com>
2021-04-13 00:31:38 -0400

    DOMSelector

--- src/js/index.js
+++ src/js/index.js
@@ -1,50 +1,101 @@
-const quizTitle = document.getElementById("quiz-title");
-const startButton = document.getElementById("start-btn");
-const nextButton = document.getElementById("next-btn");
-const doneButton = document.getElementById("done-btn");
-const quizHider = document.getElementById("element-hider");
-const scoreDisplay = document.getElementById("score");
-const answerButtonOne = document.getElementById("choice1");
-const answerButtonTwo = document.getElementById("choice2");
+DOMSelectors = {
+  quizTitle: document.getElementById("quiz-title"),
+  startButton: document.getElementById("start-btn"),
+  nextButton: document.getElementById("next-btn"),
+  doneButton: document.getElementById("done-btn"),
+  quizHider: document.getElementById("element-hider"),
+  scoreDisplay: document.getElementById("score"),
+  answerButtonOne: document.getElementById("choice1"),
+  answerButtonTwo: document.getElementById("choice2"),
+};
 const choices = [
   {
-    question: "what season were you born?",
+    question: "Rhino horns are made of keratin",
     answers: [
-      { text: "summer", correct: true },
-      { text: "winter", correct: false },
+      { text: "true", correct: true },
+      { text: "false", correct: false },
     ],
   },
   {
-    question: "what month were you born?",
+    question: "Scorpions glow under UV light",
     answers: [
-      { text: "june", correct: true },
-      { text: "july", correct: false },
+      { text: "true", correct: true },
+      { text: "false", correct: false },
     ],
   },
   {
-    question: "what day were you born?",
+    question: "Goats are cannibalistic",
     answers: [
-      { text: "19", correct: true },
-      { text: "20", correct: false },
+      { text: "true", correct: false },
+      { text: "false", correct: true },
+    ],
+  },
+  {
+    question: "A squid has 12 tentacles",
+    answers: [
+      { text: "true", correct: false },
+      { text: "false", correct: true },
+    ],
+  },
+  {
+    question: "Jellyfish are a type of a fish",
+    answers: [
+      { text: "true", correct: false },
+      { text: "false", correct: true },
+    ],
+  },
+  {
+    question: "Tigers can swim",
+    answers: [
+      { text: "true", correct: true },
+      { text: "false", correct: false },
+    ],
+  },
+  {
+    question: "Tortoises can swim well",
+    answers: [
+      { text: "true", correct: false },
+      { text: "false", correct: true },
+    ],
+  },
+  {
+    question: "Platypuses lay eggs",
+    answers: [
+      { text: "true", correct: true },
+      { text: "false", correct: false },
+    ],
+  },
+  {
+    question: "Kangaroos have been known to drown their attackers",
+    answers: [
+      { text: "true", correct: true },
+      { text: "false", correct: false },
+    ],
+  },
+  {
+    question: "Burmese Pythos are native to Florida",
+    answers: [
+      { text: "true", correct: false },
+      { text: "false", correct: true },
     ],
   },
 ];
 
 //start quiz
 
-startButton.addEventListener("click", startQuiz);
-nextButton.addEventListener("click", nextQuestion);
-answerButtonOne.addEventListener("click", choiceSelect);
-answerButtonTwo.addEventListener("click", choiceSelect);
-doneButton.addEventListener("click", resetQuiz);
+DOMSelectors.startButton.addEventListener("click", startQuiz);
+DOMSelectors.nextButton.addEventListener("click", nextQuestion);
+DOMSelectors.answerButtonOne.addEventListener("click", choiceSelect);
+DOMSelectors.answerButtonTwo.addEventListener("click", choiceSelect);
+DOMSelectors.doneButton.addEventListener("click", resetQuiz);
 
 function startQuiz() {
   console.log("started");
-  startButton.classList.add("hide"); //to hide the element
+  DOMSelectors.startButton.classList.add("hide"); //to hide the element
   //have quiz panel hidden and remove "hide" in this function
-  answerButtonOne.classList.remove("hide");
-  answerButtonTwo.classList.remove("hide");
-  nextButton.classList.remove("hide");
+  DOMSelectors.answerButtonOne.classList.remove("hide");
+  DOMSelectors.answerButtonTwo.classList.remove("hide");
+  DOMSelectors.nextButton.classList.remove("hide");
   firstQuestionIndex = 0;
   nextQuestion();
 }
@@ -54,30 +105,31 @@ function nextQuestion() {
   const questionIndex = firstQuestionIndex++; // add 1 to the first question index
   const displayQuestion = choices[questionIndex];
   const result = displayQuestion.question;
-  quizTitle.innerText = result; // displays the quesiton of the changing choice index
+  DOMSelectors.quizTitle.innerText = result; // displays the quesiton of the changing choice index
   if (questionIndex === choices.length - 1) scoreCalculator(); //once the end of the sequence is reached start the score calculator function
   const choiceA = choices[questionIndex].answers[0].text;
   const choiceB = choices[questionIndex].answers[1].text; //iterating answer 1 and 2
-  answerButtonOne.innerText = choiceA; //display answer 1
-  answerButtonTwo.innerText = choiceB; //display answer 2
+  DOMSelectors.answerButtonOne.innerText = choiceA; //display answer 1
+  DOMSelectors.answerButtonTwo.innerText = choiceB; //display answer 2
 }
 
 //when you select an answer
 function choiceSelect() {
   console.log("selected");
-  if (choices.answers.correct === true) {
+  /*if (choices.answers.correct === true) {
     i = 0;
     const quizScore = i++; //not working
-  }
+  }*/
 }
 
 //display question and answers
 
 function scoreCalculator() {
   console.log("calculated");
-  quizHider.classList.add("hide");
-  scoreDisplay.classList.remove("hide");
-  doneButton.classList.remove("hide");
+  DOMSelectors.quizHider.classList.add("hide");
+  DOMSelectors.scoreDisplay.classList.remove("hide");
+  DOMSelectors.doneButton.classList.remove("hide");
+  //scoreDisplay.textContent += quizScore;
 }
 
 function resetQuiz() {
